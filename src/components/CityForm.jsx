7a85e1c7782e39842d3c02259dce05013ac09a8e
Brandon <bran2miz@gmail.com>
2023-12-06 09:32:55 -0800

    had to fork to add changes to Steph's city explorer. Added notes, and fixed errors within her code so that it now should work

--- src/components/CityForm.jsx
+++ src/components/CityForm.jsx
@@ -1,15 +1,19 @@
-import { useState, useRef } from 'react';
+/* eslint-disable react/prop-types */
+import { useState} from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 function CityForm(props) {
   const [showHeading, setShowHeading] = useState(false);
-  const textInput = useRef(null);
 
+  // deleted the text on line 8
   const handleSubmit = (e) => {
     e.preventDefault();
     setShowHeading(true);
-    props.handleChangeCity(textInput.current.value);
+    //we no longer have to pass a value into props.handleGetLocation. The getLocation() function in App.jsx has no parameter.
+    // so when you passed down getLocation() as handleGetLocation (line 103) and implemented it into this handleSubmit() function
+    // you are just calling the function once the form is submitted or when you click on the Explore button. 
+    props.handleGetLocation();
   };
   return (
     <Form onSubmit={handleSubmit} className="google-search-form">
@@ -21,7 +25,10 @@ function CityForm(props) {
             placeholder="Oh...the places you'll go."
             size="lg"
             type="text"
-            ref={textInput}
+            // deleted the text and replaced with the onChange on 29. You have to use the
+            // updateCity() function (the one that sets searchQuery from a empty string to the e.target.value or
+            // what is typed into the form) as an onChange so that searchQuery gets a value (ie "Seattle")
+            onChange={props.updateCity}
             className="search-input"
           />
           <Button variant="primary" type="submit" className="search-button">
@@ -32,6 +39,7 @@ function CityForm(props) {
       {showHeading && props.city &&  (
         <h2 className="results-text">Welcome to {props.city}. <br/> Explore Below!</h2>
       )}
+    
     </Form>
   );
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,16 +1,19 @@
 import {When} from 'react-if';
 
 
-const API_KEY = import.meta.env.VITE_API_KEY;
+const API_KEY = 'pk.9b1f47b60a4e41bb88cb29aa817c0830'
+
+;
 
 function Map(props) {
+console.log(API_KEY);
 
-
-  
+  console.log(props.latitude)
+  console.log(props.longitude)
   return (
     <When condition={props.latitude && props.longitude}>
       <figure>
-        <img src={`https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${props.latitude},${props.longitude}&size=600x600&format=png`} width="800"/>
+        <img src={`https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${props.latitude},${props.longitude}&size=600x600&format=png`} width="800" height="800"/>
       </figure>
    
     </When>
@@ -18,3 +21,4 @@ function Map(props) {
 }
 
 export default Map;
+
